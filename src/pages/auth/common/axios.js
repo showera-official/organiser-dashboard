a6f21e1a5d5fe580f0986db3c71c39ab3424ecfb
shashank-iter <pandeyshashank812@gmail.com>
2023-07-19 02:22:54 +0530

    base url for axios setup

---
+++ src/pages/auth/common/axios.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+export default axios.create({
+  // baseURL: "https://vivacious-helmet-newt.cyclic.app/api/v1",
+  baseURL: "https://tame-teal-tortoise-wrap.cyclic.app/api/v1",
+  // baseURL: "http://127.0.0.1:4000/api/v1",
+});
