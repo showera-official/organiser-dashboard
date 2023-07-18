57ac31cdbdb30eb9bc27aead056180551310472e
shashank-iter <pandeyshashank812@gmail.com>
2023-07-19 02:22:20 +0530

    minor edits

--- src/components/partials/sidebar/MobileMenu.jsx
+++ src/components/partials/sidebar/MobileMenu.jsx
@@ -50,7 +50,7 @@ const MobileMenu = ({ className = "custom-class" }) => {
             </div>
             <div>
               <h1 className="text-xl font-semibold text-slate-900 dark:text-slate-100">
-                DashCode
+                Showera
               </h1>
             </div>
           </div>
