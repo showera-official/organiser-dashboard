e0897fa35a1aea0e1cc002a1aec54be26c047962
shashank-iter <pandeyshashank812@gmail.com>
2023-07-19 11:32:32 +0530

    logo component fixed

--- src/components/partials/sidebar/Logo.jsx
+++ src/components/partials/sidebar/Logo.jsx
@@ -7,8 +7,8 @@ import useSemiDark from "@/hooks/useSemiDark";
 import useSkin from "@/hooks/useSkin";
 
 // import images
-import MobileLogo from "@/assets/images/logo/logo-c.svg";
-import MobileLogoWhite from "@/assets/images/logo/logo-c-white.svg";
+import MobileLogo from "@/assets/images/logo/logo-c.png";
+import MobileLogoWhite from "@/assets/images/logo/logo-c-white.png";
 
 const SidebarLogo = ({ menuHover }) => {
   const [isDark] = useDarkMode();
@@ -33,16 +33,16 @@ const SidebarLogo = ({ menuHover }) => {
         <div className="flex items-center space-x-4">
           <div className="logo-icon">
             {!isDark && !isSemiDark ? (
-              <img src={MobileLogo} alt="" />
+              <img src={MobileLogo} alt="" height={40} width={50} />
             ) : (
-              <img src={MobileLogoWhite} alt="" />
+              <img src={MobileLogoWhite} alt="" height={40} width={50} />
             )}
           </div>
 
           {(!collapsed || menuHover) && (
             <div>
               <h1 className="text-xl font-semibold text-slate-900 dark:text-slate-100">
-                DashCode
+                Showera
               </h1>
             </div>
           )}
