eec4ed2357d445f146611ffa0f05ad4a07ea5ea1
shashank-iter <pandeyshashank812@gmail.com>
2023-08-03 10:51:44 +0530

    modified axios code

--- src/pages/events/DisableEventModal.jsx
+++ src/pages/events/DisableEventModal.jsx
@@ -5,29 +5,45 @@ import { Icon } from "@iconify/react";
 import Tooltip from "@/components/ui/Tooltip";
 import Cookies from "js-cookie";
 import { toast } from "react-toastify";
-import axios from "../../axios";
+import axios from "axios";
 // This is a modal to delete an event
 function DisableEventModal(props) {
   const id = props.id;
+  console.log(id);
 
   //function to toggle event status
   const toggleEventStatus = () => {
+    const token = Cookies.get("token");
+    if (!token) {
+      toast.error("Authorization token not found");
+      return;
+    }
+  
     axios
-      .put(`/event/toggle/${id}`, {
+      .put(`https://tame-teal-tortoise-wrap.cyclic.app/api/v1/event/toggle/${id}`, null, {
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${Cookies.get("token")}`,
+          Authorization: `Bearer ${token}`,
         },
       })
       .then((res) => {
-        console.log(res);
-        toast.success("Success");
+        // Check if the response contains valid JSON data
+        if (res.data) {
+          console.log(res.data);
+          toast.success("Success");
+        } else {
+          // Handle unexpected response data
+          toast.error("Unexpected response from the server");
+        }
       })
       .catch((err) => {
-        toast.error(err.response.data.message);
+        if (err.response && err.response.data && err.response.data.message) {
+          toast.error(err.response.data.message);
+        } else {
+          toast.error("An error occurred");
+        }
       });
   };
-
   return (
     <>
       <Modal
