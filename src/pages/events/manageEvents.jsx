96f23ccfe74e75b174e0c79c29379fbc5b0508d6
shashank-iter <pandeyshashank812@gmail.com>
2023-08-03 10:22:59 +0530

    manage event window's UI modified

--- src/pages/events/manageEvents.jsx
+++ src/pages/events/manageEvents.jsx
@@ -7,27 +7,35 @@ import Cookies from "js-cookie";
 import Loading from "@/components/Loading";
 import { Icon } from "@iconify/react";
 import EditEvent from "./EditEvent";
+import { Link } from "react-router-dom";
+import Tooltip from "@/components/ui/Tooltip";
+import DisableEventModal from "./DisableEventModal";
+
 // import loader and use it before rendering the data.
 const columns = [
   {
-    label: "ID",
-    field: "age",
+    label: "Event Name",
+    field: "event_name",
   },
   {
-    label: "Event Name",
+    label: "Event Created On",
     field: "first_name",
   },
 
   {
-    label: "Email",
-    field: "email",
+    label: "Event Status",
+    field: "event_is_active",
+  },
+  {
+    label: "Action",
+    field: "action",
   },
 ];
 const rows = tableData.slice(0, 7);
 
 export default function manageEvents() {
   const [field, setFields] = useState(null);
-
+ 
   useEffect(() => {
     axios
       .get("/event", {
@@ -42,6 +50,10 @@ export default function manageEvents() {
         setFields(res.data.data);
       });
   }, []);
+
+
+
+
   return !field ? (
     <Loading />
   ) : (
@@ -66,16 +78,58 @@ export default function manageEvents() {
                       key={_id}
                       className="hover:bg-slate-200 dark:hover:bg-slate-700"
                     >
-                      <td className="table-td">{field._id}</td>
                       <td className="table-td">{field.event_name}</td>
-                      <td className="table-td ">{field.event_start_date}</td>
+                      <td className="table-td">{field.createdAt.split("T")[0]}</td>
+                      <td className="table-td ">
+                      <span className="block w-full">
+          <span
+            className={` inline-block px-3 min-w-[90px] text-center mx-auto py-1 rounded-[999px] bg-opacity-25 ${
+              field.event_is_active === 1
+                ? "text-success-500 bg-success-500"
+                : ""
+            } 
+            ${
+             field.event_is_active === 0
+                ? "text-warning-500 bg-warning-500"
+                : ""
+            }
+           
+            
+             `}
+          >
+            {field.event_is_active === 1 ? "Active" : "Disabled"}
+          </span>
+        </span>
+                      </td>
                       <td className="table-td">
-                        <Icon
-                          icon="heroicons-outline:information-circle"
-                          onClick={() => {
-                            <EditEvent field={field} />;
-                          }}
-                        />
+                        <div className="flex flex-row gap-x-1 align-middle">
+                          <div className="w-1/2 justify-center">
+                            <Link to={`/editevent/${field._id}`}>
+                              <Tooltip
+                                title={
+                                  <>
+                                    <Icon
+                                      icon="heroicons-outline:pencil"
+                                      className="w-5 h-5 text-slate-800 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-300 cursor-pointer"
+                                    />
+                                  </>
+                                }
+                                content="Edit"
+                                placement="top"
+                                theme="dark"
+                                className=" dark:text-white hover:text-white dark:hover:text-black"
+                                arrow
+                              />
+                            </Link>
+                          </div>
+                          <div className="w-1/2">
+                            <DisableEventModal
+                              id={field._id}
+                              name={field.event_name}
+                          icon={field.event_is_active == 1? "heroicons-outline:bookmark" : "heroicons-outline:bookmark-slash"}
+                            />
+                          </div>
+                        </div>
                       </td>
                     </tr>
                   ))}
